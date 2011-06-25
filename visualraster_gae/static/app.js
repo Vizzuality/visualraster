c3b91867e159e35f2713978a09a801474a02c086
javi <qualopec@gmail.com>
2011-06-25 11:33:42 +0200

    some refactoring

--- visualraster_gae/static/app.js
+++ visualraster_gae/static/app.js
@@ -1,55 +1,59 @@
 
-function filter(image_data, w, h, threshold) {
-    var components = 4; //rgba
-    var pixel_pos;
-    for(var i=0; i < w; ++i) {
-        for(var j=0; j < h; ++j) {
-            var pixel_pos = (j*w + i) * components;
-            if(image_data[pixel_pos] < threshold) {
-                image_data[pixel_pos] = image_data[pixel_pos + 1] = image_data[pixel_pos + 2] = 0;
-                image_data[pixel_pos + 3] = 0;
-            }
-        }
-    }
-};
 
-var App = {
-    threshold: 0,
-    init: function(layer) {  
-        var chicago = new google.maps.LatLng(41.850033,-87.6500523);
-        var mapOptions = {
-            zoom: 3,
-            center: chicago,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+var App = function() {
+        var me = {
+            mapOptions: {
+                zoom: 3,
+                center: new google.maps.LatLng(41.850033,-87.6500523),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
         };
-        map = new google.maps.Map(document.getElementById("map"), mapOptions);
-        this.heightLayer = layer || new CanvasTileLayer(App.canvas_setup, filter);
-        map.overlayMapTypes.insertAt(0, this.heightLayer);
-        App.map = map;
-        this.setup_ui();
-    },
 
-    setup_ui: function() {
-        $("#slider").slider({
-            slide: function(event, ui) {  
-                $("#slider_value").html(8000*ui.value/100.0 + " meters");
-                App.threshold = ui.value;
-                App.heightLayer.filter_tiles(ui.value);
+        function filter(image_data, w, h, threshold) {
+            var components = 4; //rgba
+            var pixel_pos;
+            for(var i=0; i < w; ++i) {
+                for(var j=0; j < h; ++j) {
+                    var pixel_pos = (j*w + i) * components;
+                    if(image_data[pixel_pos] < threshold) {
+                        image_data[pixel_pos] = image_data[pixel_pos + 1] = image_data[pixel_pos + 2] = 0;
+                        image_data[pixel_pos + 3] = 0;
+                    }
+                }
             }
-        });
-    },
+        };
 
-    canvas_setup: function(canvas, coord, zoom) {
-      var image = new Image();  
-      var ctx = canvas.getContext('2d');
-      image.src = "/proxy/mountainbiodiversity.org/env/z" + zoom + "/"+ coord.x + "/" + coord.y +".png";
-      canvas.image = image;
-      $(image).load(function() { 
-            //ctx.globalAlpha = 0.5;
-            ctx.drawImage(image, 0, 0);  
-            App.heightLayer.filter_tile(canvas, [App.threshold]);
-      });
-    }
+        function canvas_setup(canvas, coord, zoom) {
+          var image = new Image();  
+          var ctx = canvas.getContext('2d');
+          image.src = "/proxy/mountainbiodiversity.org/env/z" + zoom + "/"+ coord.x + "/" + coord.y +".png";
+          canvas.image = image;
+          $(image).load(function() { 
+                //ctx.globalAlpha = 0.5;
+                ctx.drawImage(image, 0, 0);  
+                App.heightLayer.filter_tile(canvas, [App.threshold]);
+          });
+        }
 
-}
+        me.init = function(layer) {
+            var map = new google.maps.Map(document.getElementById("map"), this.mapOptions);
+            this.heightLayer = layer || new CanvasTileLayer(canvas_setup, filter);
+            map.overlayMapTypes.insertAt(0, this.heightLayer);
+            this.map = map;
+            this.setup_ui();
+        }
+
+        me.setup_ui = function() {
+            var that = this;
+            $("#slider").slider({
+                slide: function(event, ui) {  
+                    $("#slider_value").html(8000*ui.value/100.0 + " meters");
+                    that.threshold = ui.value;
+                    that.heightLayer.filter_tiles(ui.value);
+                }
+            });
+        }
+        return me;
+    }();
+
 
